ff6e374c0c961cfa45a16e6d538be24632101cec
Hannah Krager <hannahkrager@gmail.com>
2016-04-06 20:29:19 -0700

    add depth function

--- src/bst.py
+++ src/bst.py
@@ -4,32 +4,84 @@ class BST(object):
 
     def __init__(self):
         """Instantiating tree and setting marker to show if tree is empty."""
-        self._marker = object()
-        self.head = self._marker
+        # self._marker = object()
+        self.head = None
+        self.length = 0
+
+    def _get_node(self, value):
+        curnode = self.head
+        while curnode.get_left_child() or curnode.get_right_child():
+            if value < curnode.data and curnode.get_left_child():
+                curnode = curnode.get_left_child()
+            elif value > curnode.data and curnode.get_right_child():
+                curnode = curnode.get_right_child()
+            else:
+                break
+        return curnode
 
     def insert(self, value):
         """Add a node to the tree in the appropriate place."""
         if isinstance(value, int):
-            if self.head == self._marker:
+            if self.length == 0:
                 self.head = BSTNode(value)
+                self.length += 1
                 return
-            curnode = self.head
-            while curnode.get_left_child() or curnode.get_right_child():
-                if value < curnode.data and curnode.get_left_child():
-                    curnode = curnode.get_left_child()
-                elif value > curnode.data and curnode.get_right_child():
-                    curnode = curnode.get_right_child()
-                else:
-                    break
+            curnode = self._get_node(value)
             if value < curnode.data:
                 curnode.set_left_child(BSTNode(value, curnode))
+                self.length += 1
             elif value > curnode.data:
                 curnode.set_right_child(BSTNode(value, curnode))
+                self.length += 1
             else:
                 raise ValueError('That value is already in the tree.')
 
         else:
             raise ValueError("You cannot insert {}".format(type(value)))
+
+    def size(self):
+        """Return the number of nodes in the tree."""
+        return self.length
+
+    def contains(self, value):
+        """Check if given value is already in tree."""
+        curnode = self._get_node(value)
+        if curnode.data == value:
+            return True
+        return False
+
+
+    def depth(self):
+        """Calculate the depth of the tree."""
+        if not self.length:
+            return 0
+        visited = [None]
+        queue = [self.head]
+        depth = 1
+        cur_depth = 1
+
+        while queue:
+
+            while queue[-1].get_right_child() not in visited or queue[-1].get_left_child() not in visited:
+                lc = queue[-1].get_left_child()
+                rc = queue[-1].get_right_child()
+                if rc not in visited:
+                    queue.append(rc)
+                    visited.append(rc)
+                    cur_depth += 1
+                    if depth < cur_depth:
+                        depth = cur_depth
+                elif lc not in visited:
+                    queue.append(lc)
+                    visited.append(lc)
+                    cur_depth += 1
+                    if depth < cur_depth:
+                        depth = cur_depth
+            queue.pop()
+            cur_depth -= 1
+        return depth
+
+
 
 
 class BSTNode(object):
--- src/test_bst.py
+++ src/test_bst.py
@@ -1,14 +1,30 @@
 # _*_ encoding: utf-8 _*_
 import pytest
 
+NODES_IN_TREE = [(20, True), (22, True), (21, True), (14, True), (75, False),
+                 (0, False), (7, False), (15, False)]
 
-# new_bst = [(10)]
 
-# @pytest.mark.parametrize()
+@pytest.fixture()
+def populated_tree():
+    """Fixture for populating BST."""
+    from bst import BST
+    new_bst = BST()
+    new_bst.insert(20)
+    new_bst.insert(22)
+    new_bst.insert(14)
+    new_bst.insert(17)
+    new_bst.insert(21)
+    new_bst.insert(3)
+    new_bst.insert(6)
+
+    return new_bst
+
+
 def test_new_bst():
     from bst import BST
     new_bst = BST()
-    assert new_bst.head
+    assert not new_bst.head
 
 
 def test_first_item():
@@ -47,3 +63,26 @@ def test_insert_same():
     new_bst.insert(5)
     with pytest.raises(ValueError):
         new_bst.insert(5)
+
+
+@pytest.mark.parametrize('value, result', NODES_IN_TREE)
+def test_contains(populated_tree, value, result):
+    assert populated_tree.contains(value) == result
+
+
+def test_not_contains(populated_tree):
+    assert not populated_tree.contains(1)
+
+
+def test_size_empty():
+    from bst import BST
+    new_bst = BST()
+    assert new_bst.size() == 0
+
+
+def test_size_not_empty(populated_tree):
+    assert populated_tree.size() == 7
+
+
+def test_depth(populated_tree):
+    assert populated_tree.depth() == 4
